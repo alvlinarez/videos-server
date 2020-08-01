c4c65338eb4b798f04aeced81bb47658cde2ec2f
Alvaro Linarez <alvarolinarez9@gmail.com>
2020-08-01 17:09:36 -0500

    Index moved to src folder

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 
 // environment variables
-const config = require('./src/config/env');
+const config = require('./config/env');
 // DB connection
-const connectionDB = require('./src/config/db');
+const connectionDB = require('./config/db');
 // routes
-const authRoutes = require('./src/routes/auth');
-const userRoutes = require('./src/routes/users');
-const movieRoutes = require('./src/routes/movies');
-const playlistRoutes = require('./src/routes/playlists');
-const ratingRoutes = require('./src/routes/ratings');
-const tagRoutes = require('./src/routes/tags');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+const movieRoutes = require('./routes/movies');
+const playlistRoutes = require('./routes/playlists');
+const ratingRoutes = require('./routes/ratings');
+const tagRoutes = require('./routes/tags');
 
 // Custom middleware
-const notFoundHandler = require('./src/utils/middleware/notFoundHandler');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 // Initializing connection to DB
 connectionDB();
